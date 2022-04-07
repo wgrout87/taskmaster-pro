75735c3b0437727f43bf9237d16f1766ab892738
Will G <wgrout87@gmail.com>
2022-04-07 09:10:23 -0600

    Added datepicker and color changes by due date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 var tasks = {};
 
+$("#modalDueDate").datepicker({
+    minDate: 1
+});
+
 var createTask = function (taskText, taskDate, taskList) {
     // create elements that make up a task item
     var taskLi = $("<li>").addClass("list-group-item");
+
     var taskSpan = $("<span>")
         .addClass("badge badge-primary badge-pill")
         .text(taskDate);
+
     var taskP = $("<p>")
         .addClass("m-1")
         .text(taskText);
@@ -13,6 +19,8 @@ var createTask = function (taskText, taskDate, taskList) {
     // append span and p element to parent li
     taskLi.append(taskSpan, taskP);
 
+    // check due date
+    auditTask(taskLi);
 
     // append to ul list on the page
     $("#list-" + taskList).append(taskLi);
@@ -99,12 +107,22 @@ $(".list-group").on("click", "span", function () {
     // swap out elements
     $(this).replaceWith(dateInput);
 
+    // enable jquery ui datepicker
+    dateInput.datepicker({
+        minDate: 1,
+
+        onClose: function () {
+            // When calendar is closed, force a "change" event on the 'dateInput'
+            $(this).trigger("change");
+        }
+    });
+
     // automatically focus on new element
     dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
     // get current text
     var date = $(this)
         .val()
@@ -132,6 +150,9 @@ $(".list-group").on("blur", "input[type='text']", function () {
 
     // replace input with span element
     $(this).replaceWith(taskSpan);
+
+    // Pass task's <li> element into auditTask() to check new due date
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
@@ -244,6 +265,25 @@ $("#remove-tasks").on("click", function () {
     }
     saveTasks();
 });
+
+var auditTask = function (taskEl) {
+    // get date from task element
+    var date = $(taskEl).find("span").text().trim();
+
+    // convert to moment object at 5:00pm
+    var time = moment(date, "L").set("hour", 17);
+
+    // remove any old classes from element
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+    // apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+        $(taskEl).addClass("list-group-item-danger");
+    }
+    else if (Math.abs(moment().diff(time, "days")) <= 2) {
+        $(taskEl).addClass("list-group-item-warning");
+    }
+};
 
 // load tasks for the first time
 loadTasks();
