d78bb70ec876b7f131ddccd4a623ae9aae55c3f8
Will G <wgrout87@gmail.com>
2022-04-10 14:16:31 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,7 +54,6 @@ var saveTasks = function () {
 
 $(".list-group").on("click", "p", function () {
     var text = $(this).text();
-    console.log(text);
     var textInput = $("<textarea>")
         .addClass("form-control")
         .val(text);
@@ -173,16 +172,18 @@ $(".card .list-group").sortable({
     tolerance: "pointer",
     helper: "clone",
     activate: function (event) {
-        console.log("activate", this);
+        $(this).addClass("dropover");
+        $(".bottom-trash").addClass("bottom-trash-drag");
     },
     deactivate: function (event) {
-        console.log("deactivate", this);
+        $(this).removeClass("dropover");
+        $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function (event) {
-        console.log("over", event.target);
+        $(event.target).addClass("dropover-active");
     },
     out: function (event) {
-        console.log("out", event.target);
+        $(event.target).removeClass("dropover-active");
     },
     update: function (event) {
         // array to store the task data in
@@ -228,15 +229,15 @@ $("#trash").droppable({
         ui.draggable.remove();
     },
     over: function (event, ui) {
-        console.log("over");
+        $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function (event, ui) {
-        console.log("out");
+        $(".bottom-trash").removeClass("bottom-trash-active");
     }
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
     // get form values
     var taskText = $("#modalTaskDescription").val();
     var taskDate = $("#modalDueDate").val();
@@ -287,5 +288,11 @@ var auditTask = function (taskEl) {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function () {
+    $(".card .list-group-item").each(function (index, el) {
+        auditTask(el);
+    });
+}, (1000 * 60) * 30);
 
 
